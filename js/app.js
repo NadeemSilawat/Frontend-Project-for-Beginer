19f83c1d13763bf5800126343d4820aebebe1ef4
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-02-24 13:41:16 +0530

    JS

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const getElement = (selector) => {
   const element = document.querySelector(selector)
 
-  if (element) return element
+  if (element) return element     
   throw Error(
     `Please double check your class names, there is no ${selector} class`
   )
